90ffacd6b79e75a5ed657a50816ffd5b59db6f3d
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-15 22:40:22 -0300

    Test: app fixture changed to SQLAlchemy and runner fixture removed.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,26 +3,23 @@ import tempfile
 
 import pytest
 from app import create_app
-from app.db import get_db, init_db
-
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
+from app.db import db
 
 @pytest.fixture
 def app():    
 
-    db_fd, db_path = tempfile.mkstemp()
-
+    db_fd, db_path = tempfile.mkstemp()    
+    
     app = create_app({
-        'TESTING': True,
-        'DATABASE': db_path,
-    })    
-
-    with app.app_context():
-        init_db()
-        get_db().executescript(_data_sql)
-
-    yield app
+        'TESTING': True,        
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///{}'.format(db_path),
+        'SQLALCHEMY_TRACK_MODIFICATIONS': False
+    })        
+    
+    app.app_context().push()
+    db.create_all()
+    
+    yield app    
 
     os.close(db_fd)
     os.unlink(db_path)
@@ -31,8 +28,3 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
-
-
-@pytest.fixture
-def runner(app):
-    return app.test_cli_runner()    
