f8c1284926a11fe58658f87f608bf6dbaad65555
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-15 23:37:46 -0300

    feat: add no blueprint Auth a rota a ser usada no registro de novos usuários

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,10 +1,11 @@
-import functools
+from flask import Blueprint
 
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from werkzeug.security import check_password_hash, generate_password_hash
-
-from app.db import get_db
+from app.controllers.authcontroller import AuthController
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=['POST'])
+def register():
+    return AuthController().register()
+
+    
