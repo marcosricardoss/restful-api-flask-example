6d2e8216205b91d5bafdc7af3831b5fdf33889cb
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-15 23:28:37 -0300

    fix: fix in database file path

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,10 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',        
-        SQLALCHEMY_DATABASE_URI='sqlite:///database.sqlite3',
+        SQLALCHEMY_DATABASE_URI='sqlite:///{}/database.sqlite3'.format(app.instance_path),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
-    )
-
+    )    
+    
     db.init_app(app)
 
     if test_config is None:
@@ -22,7 +22,7 @@ def create_app(test_config=None):
         db.create_all()
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        app.config.from_mapping(test_config)    
 
     # ensure the instance folder exists
     try:
