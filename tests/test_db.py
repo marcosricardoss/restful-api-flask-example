c8bc9551a10e3acb1513cfda3dcc7178b8fe988f
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-15 22:48:32 -0300

    test: change in database test

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,28 +1,5 @@
-import sqlite3
+from app.db import db
 
-import pytest
-from app.db import get_db
-from app.db import get_db, init_db
-
-def test_get_close_db(app):
-    with app.app_context():
-        db = get_db()
-        assert db is get_db()
-
-    with pytest.raises(sqlite3.ProgrammingError) as e:
-        db.execute('SELECT 1')
-
-    assert 'closed' in str(e)
-
-
-def test_init_db_command(runner, monkeypatch):
-    class Recorder(object):
-        called = False
-
-    def fake_init_db():
-        Recorder.called = True
-
-    monkeypatch.setattr('app.db.init_db', fake_init_db)
-    result = runner.invoke(args=['init-db'])
-    assert 'Initialized' in result.output
-    assert Recorder.called    
+def test_user_table_created(app):
+    result = db.engine.execute('SELECT null FROM user LIMIT 1')
+    assert result
