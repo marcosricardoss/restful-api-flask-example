387f692cafe07ccb50a3fe2d67851abf53267cc1
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-17 13:53:07 -0300

    test: change in the DatabasePopulator to add in database administrator users.

--- tests/database_populator.py
+++ tests/database_populator.py
@@ -8,9 +8,9 @@ from app.db import User
 
 class DatabasePopulator:
 
-    def create_user(username, password):
+    def create_user(username, password, admin):
         hashed_password = generate_password_hash(password, method='sha256')
-        user = User(public_id=str(uuid.uuid4()), username=username, password=hashed_password)
+        user = User(public_id=str(uuid.uuid4()), username=username, password=hashed_password, admin=admin)
         db.session.add(user)
         db.session.commit()
 
@@ -18,5 +18,6 @@ class DatabasePopulator:
 
     @staticmethod
     def populate():
-        user1 = DatabasePopulator.create_user("tester1", "secret")
-        user2 = DatabasePopulator.create_user("tester2", "secret")
+        user1 = DatabasePopulator.create_user("tester1", "secret", False)
+        user2 = DatabasePopulator.create_user("tester2", "secret", False)
+        admin1 = DatabasePopulator.create_user("admin1", "secret", True)
