d4cb0584444af8187708b2969a47a431f0bdd132
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-17 13:15:40 -0300

    feat: add in the 'User' model the 'admin' column to indicate user admins

--- app/db.py
+++ app/db.py
@@ -9,6 +9,7 @@ class User(db.Model):
     public_id = db.Column(db.String(50), unique=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=True, nullable=False)
+    admin = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
