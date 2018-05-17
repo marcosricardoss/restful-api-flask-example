659ddc8e7f97239a6908d1619153cc738dc6eb2c
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-17 13:54:46 -0300

    test: add test to administrator users.

--- tests/test_auth_register.py
+++ tests/test_auth_register.py
@@ -6,7 +6,7 @@ from app.db import db
 from app.db import User
 from database_populator import DatabasePopulator
 
-def test_register_returns_rtatus_code_201_with_msg_when_unique_username_and_password_is_passed(client):
+def test_register_returns_status_code_201_with_msg_when_unique_username_and_password_is_passed(client):
 
     username = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
     hashed_password = generate_password_hash("secret", method='sha256')
@@ -15,10 +15,46 @@ def test_register_returns_rtatus_code_201_with_msg_when_unique_username_and_pass
         'username': username, 
         'password': hashed_password
     })
+
     result = response.get_json()
+    user = User.query.filter_by(username=username).first()
     assert response.status_code == 201    
     assert result['message'] == "User successfully created." 
+    assert not user.admin
 
+def test_register_returns_status_code_201_with_msg_when_unique_username_password_and_any_admin_value_is_passed(client):
+
+    username = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
+    hashed_password = generate_password_hash("secret", method='sha256')
+
+    response = client.post('/auth/register', json={
+        'username': username, 
+        'password': hashed_password,
+        'admin': 'xxxx'
+    })
+
+    result = response.get_json()
+    user = User.query.filter_by(username=username).first()
+    assert response.status_code == 201    
+    assert result['message'] == "User successfully created." 
+    assert not user.admin
+
+def test_register_admin_returns_status_code_201_with_msg_when_unique_username_password_is_passed(client):
+
+    username = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
+    hashed_password = generate_password_hash("secret", method='sha256')
+
+    response = client.post('/auth/register', json={
+        'username': username, 
+        'password': hashed_password,
+        'admin': "true"
+    })
+
+    result = response.get_json()    
+    user = User.query.filter_by(username=username).first()
+    assert response.status_code == 201
+    assert result['message'] == "User successfully created." 
+    assert user.admin
 
 def test_register_returns_status_code_400_with_msg_when_only_username_is_passed(client):
 
@@ -27,6 +63,7 @@ def test_register_returns_status_code_400_with_msg_when_only_username_is_passed(
     response = client.post('/auth/register', json={
         'username': username        
     })
+
     result = response.get_json()
     assert response.status_code == 400
     assert result['message'] == "Username or Password field not provided." 
@@ -39,6 +76,7 @@ def test_register_returns_status_code_400_with_msg_when_only_password_is_passed(
     response = client.post('/auth/register', json={
         'password': hashed_password
     })
+
     result = response.get_json()
     assert response.status_code == 400
     assert result['message'] == "Username or Password field not provided."     
@@ -76,7 +114,7 @@ def test_register_returns_status_code_400_with_msg_when_username_and_password_is
     assert result['message'] == "Username or Password field not provided."                 
 
 
-def test_register_returns_status_code_409_with_msg_when_already_existing_username_with_password_is_passed(client):    
+def test_register_returns_status_code_409_with_msg_when_already_existing_username_with_password_is_passed(client):
 
     DatabasePopulator.populate()
 
