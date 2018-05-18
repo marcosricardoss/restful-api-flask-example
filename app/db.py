018cc8c6cd748802750e7bb098bd379c988e991f
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-17 21:32:16 -0300

    feat: add method to serialize the User model data

--- app/db.py
+++ app/db.py
@@ -11,5 +11,14 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=True, nullable=False)
     admin = db.Column(db.Boolean, default=False, nullable=False)
 
+    @property
+    def serialize(self):
+        data = {            
+            'public_id': self.public_id,
+            'username': self.username,
+            'admin': self.admin
+        }
+        return data      
+
     def __repr__(self):
         return '<User %r>' % self.username
