e0f8b30798ac7fb1d29f2dd3a7d7900b746f17cf
Marcos Ricardo <marcosricardoss@gmail.com>
2018-05-17 21:08:26 -0300

    feat: add handler for 400, 404 and TypeError errors

---
+++ app/handler.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+
+def handlers(app):
+
+    @app.errorhandler(400)
+    def handle_invalid_usage(error):        
+        output = { "message": str(error) }
+        res = jsonify(output)
+        res.status_code = 404
+        return res
+    
+    @app.errorhandler(404)
+    def handle_invalid_usage(error):        
+        output = { "message": str(error) }
+        res = jsonify(output)
+        res.status_code = 404
+        return res           
+
+    @app.errorhandler(TypeError)
+    def handle_invalid_usage(error):        
+        output = { "message": str(error) }
+        res = jsonify(output)
+        res.status_code = 400
+        return res
